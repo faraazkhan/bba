c995a4bd50ba99c17d871fe06f4aaa21611ff9e4
Faraaz Khan <faraaz@rationalizeit.us>
2011-12-03 15:48:07 -0500

    first commit for better business analysis

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bba::Application.config.secret_token = '172f32f76dc79d54536bc92e0ac370291bef0eac72580eb11f9c009afe77c4633fcf3bb56c93819b767bb8d2f3f1c673208c69b231fe4180b628b367a757320a'
